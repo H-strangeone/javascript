7ba52f975cd83efb99ba7f480f6db86142333dcb
H-strangeone <hiteshgitman@gmail.com>
2024-01-12 23:33:01 -0800

    v

--- morewithobjects.js
+++ morewithobjects.js
@@ -6,3 +6,6 @@ const me={
         return 2023-birthyear;//anyfunction attached to an object is called an method and this is the only way to declare it
     }
 }
+//two ways to call this method 
+console.log(me.calcage(2005));
+console.log(me[calcage](2005));//you dont put the 2005 in the[]to pass it to the method here for objects

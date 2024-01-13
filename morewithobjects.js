17c6c57a4208a881d711c7306909c970d5e17421
H-strangeone <hiteshgitman@gmail.com>
2024-01-12 23:47:07 -0800

    i

--- morewithobjects.js
+++ morewithobjects.js
@@ -1,22 +1,39 @@
-// const me={
-//     name: 'hitesh',
-//     age: 19,
-//     birthyear:2004,
-//     calcage: function(birthyear){
-//         return 2023-birthyear;//anyfunction attached to an object is called an method and this is the only way to declare it
-//     }
-// }
-// //two ways to call this method 
-// console.log(me.calcage(2005));
-// console.log(me['calcage'](2005));//you dont put the 2005 in the[]to pass it to the method here for objects
+const me={
+    name: 'hitesh',
+    age: 19,
+    birthyear:2004,
+    calcage: function(birthyear){
+        return 2023-birthyear;//anyfunction attached to an object is called an method and this is the only way to declare it
+    }
+}
+//two ways to call this method 
+console.log(me.calcage(2005));
+console.log(me['calcage'](2005));//you dont put the 2005 in the[]to pass it to the method here for objects
+
 const me={
     name: 'hitesh',
     age: 19,
     birthyear:2004,
     calcage: function(){
-        return 2023-this.birthyear;//here this will  access the birthyear given  in the object so we wont have to pass some input to this method any time now we call it
+        console.log(this)//this will basically print everything in thee object because of the keyword this can just use me too
+        return 2023-this.birthyear;//here this will  access the birthyear given  in the object so we wont have to pass some input to this method any time now we call it, can use me.birthyear too
     }
 }
 //two ways to call this method 
-console.log(me.calcage);
+console.log(me.calcage());
+console.log(me['calcage']);
+
+const me={
+    name: 'hitesh',
+    age: 19,
+    birthyear:2004,
+    calcage: function(){
+        
+        // console.log(this)//this will basically print everything in thee object because of the keyword this can just use me too
+        this.age= 2023-this.birthyear;
+        return this.age;//here this will  access the birthyear given  in the object so we wont have to pass some input to this method any time now we call it, can use me.birthyear too
+    }
+}
+//two ways to call this method 
+console.log(me.calcage());
 console.log(me['calcage']);

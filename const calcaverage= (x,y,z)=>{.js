931db3d9bf65d8580f1e9f6e08560e1e345cd3e4
H-strangeone <hiteshgitman@gmail.com>
2024-01-13 00:06:46 -0800

    mod

--- const calcaverage= (x,y,z)=>{.js
+++ const calcaverage= (x,y,z)=>{.js
@@ -5,10 +5,10 @@ const scoreDolphins=calcaverage(44,23,71)
 const scoreKoalas=calcaverage(65,54,49)
 function checkWinner(x,y){
     if(x>y){
-        console.log('Dolphins win (${x} vs ${y}');
+        console.log(`Dolphins win (${x} vs ${y}`);
     }
     else if(y>x){
-        console.log("Koalas win (${y} vs ${x}");
+        console.log(`Koalas win (${y} vs ${x}`);
     }
     else{console.log("no team wins...")}
 }
--- morewithobjects.js
+++ morewithobjects.js
@@ -23,17 +23,27 @@ const me={
 console.log(me.calcage());
 console.log(me['calcage']);
 
-const me={
+const me = {
     name: 'hitesh',
     age: 19,
-    birthyear:2004,
-    calcage: function(){
-        
-        // console.log(this)//this will basically print everything in thee object because of the keyword this can just use me too
-        this.age= 2023-this.birthyear;
-        return this.age;//here this will  access the birthyear given  in the object so we wont have to pass some input to this method any time now we call it, can use me.birthyear too
+    birthyear: 2004,
+    hasdrivers: false,
+    calcage: function () {
+        this.age = 2023 - this.birthyear;
+        return this.age;
+    },
+    getsummary: function () {
+        if (this.hasdrivers) {
+            console.log(`${this.name} is ${this.calcage()} years old and he was born in ${this.birthyear} and he has a drivers license`);
+        }
+        else {
+            console.log(`${this.name} is ${this.calcage()} years old and he was born in ${this.birthyear} and he has no drivers license`);
+        }
+
     }
 }
+console.log(me.getsummary())
 //two ways to call this method 
 console.log(me.calcage());
 console.log(me['calcage']);
+// windows+d selects all the same words to the  word where the cursor is pointing now like agar abhi cursor me par hai aur maine ye command dabaya to saare me select ho jaayenge

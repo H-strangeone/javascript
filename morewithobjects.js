de3f62f072d50ea1665e76890cf1a812edcaefb6
H-strangeone <hiteshgitman@gmail.com>
2024-01-12 23:29:26 -0800

    comment

--- morewithobjects.js
+++ morewithobjects.js
@@ -3,6 +3,6 @@ const me={
     age: 19,
     birthyear:2004,
     calcage: function(birthyear){
-        return 2023-birthyear;//anyfunction attached to an object is called an method
+        return 2023-birthyear;//anyfunction attached to an object is called an method and this is the only way to declare it
     }
 }

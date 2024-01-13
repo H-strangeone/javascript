75ac0803f47a68911e4bff965c01bfbf8f4bacb5
H-strangeone <hiteshgitman@gmail.com>
2024-01-12 23:28:22 -0800

    i

---
+++ morewithobjects.js
@@ -0,0 +1,8 @@
+const me={
+    name: 'hitesh',
+    age: 19,
+    birthyear:2004,
+    calcage: function(birthyear){
+        return 2023-birthyear;//anyfunction attached to an object is called an method
+    }
+}
